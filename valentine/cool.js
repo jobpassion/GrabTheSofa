59f17e41179b12dcc4945c5775c9496a532ff64c
jobpassion <jobpassion@gmail.com>
2013-08-13 00:28:24 +0800

    Signed-off-by: jobpassion <jobpassion@gmail.com>

--- valentine/cool.js
+++ valentine/cool.js
@@ -1,4 +1,4 @@
-var ques = [{q:'Q.1:The famous painter', a:'Leonardo Di Ser Piero Da Vinci'},{q:'Q.2:A man,who is the most handsome', a:'jiajinping'},{q:'Q.3:Which keyword to statement function', a:'function'},{q:'Q.4:In which year was China founded', a:'1949'},{q:'Q.5:A fruit, you eat recently', a:'banana'},{q:'Q.6:Who said love you', a:'jiajinping'},{q:'Q.7:What do you guess,he send you', a:'chocolate'},{q:'Q.8:will he send you chocolate later？', a:'yes'},{q:'Q.9:Do you miss him?', a:'yes'},{q:'Q.10:your birthday', a:'0709'},{q:'Q.11:What\'s the date you get together', a:'0729'},{q:'Q.13:which program language he skilled in?', a:'java'},{q:'Q.14:Ah, you are a clever girl?', a:'yes'}];
+var ques = [{q:'Q.1:The famous painter', a:'Leonardo Di Ser Piero Da Vinci'},{q:'Q.2:A man,who is the most handsome', a:'jiajinping'},{q:'Q.3:Which keyword to statement function', a:'function'},{q:'Q.4:In which year was China founded', a:'1949'},{q:'Q.5:A fruit, you eat recently', a:'banana'},{q:'Q.6:Who said love you', a:'jiajinping'},{q:'Q.7:What do you guess,he send you', a:'chocolate'},{q:'Q.8:will he send you chocolate later？', a:'yes'},{q:'Q.9:Do you miss him?', a:'yes'},{q:'Q.10:your birthday', a:'0709'},{q:'Q.11:What\'s the date you get together', a:'0729'},{q:'Q.12:which program language he skilled in?', a:'java'},{q:'Q.13:Ah, you are a clever girl?', a:'yes'}];
 var welcome = 'welcomewelcomewelcomewelcome';
 var quesIndex;
 var flag = false;
@@ -138,6 +138,9 @@ setTimeout(function(){
 							inputType($('#finput').val('')[0], 'Congratulations.Love you, prety girl ✌', end);
 						}
 						end();
+						
+						var au = $('<audio autoplay="autoplay" loop="loop" src="' + chrome.extension.getURL('valentine/weileai.mp3') + '" />');
+						$('body').append(au);
 					}
 					quesIndex++;
 					chrome.storage.local.set({volentineIndex:quesIndex});
@@ -156,7 +159,7 @@ setTimeout(function(){
 
 
 
-					//chrome.storage.local.set({volentineIndex:0});
+					//chrome.storage.local.set({volentineIndex:12});
 
 chrome.storage.local.get('volentineIndex', function(newsVar){
 	quesIndex = newsVar['volentineIndex'];
@@ -166,3 +169,4 @@ chrome.storage.local.get('volentineIndex', function(newsVar){
 	if(quesIndex < ques.length)
 		r_init();
 });
+
